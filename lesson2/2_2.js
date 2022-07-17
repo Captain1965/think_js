88da43dab0177506a885ad71aaa60c0c6972f75b
Captain1965 <alex1965-03@mail.ru>
2022-07-17 10:34:26 +0300

    change task 2_2

--- lesson2/2_2.js
+++ lesson2/2_2.js
@@ -1,12 +1,11 @@
 // Реализовать функцию для сравнения двух массивов, 
 // сравнение должно попарно сравнивать каждый элемент 
-function compareArray (array1,array2) {
- array1.map(function callback(element1,index) {console.log(element1 == array2[index])
- })
-}
  array1 = [1,3,5,10]
  array2 = [1,3,4,5]
-compareArray(array1,array2)
-compareArray ([1,2], [2,1])
+console.log(compareArray(array1,array2))
+console.log(compareArray ([1,2], [2,1]))
 
-
+function compareArray (array1,array2) { 
+  return array1.map(function callback(item,index) {return item == array2[index]})
+    }
+ 
