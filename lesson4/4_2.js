30a6ad1514b71be1566d43c3a238766707eb3d42
Captain1965 <alex1965-03@mail.ru>
2022-08-24 02:32:24 +0300

    lesson4 variant 3

--- lesson4/4_2.js
+++ lesson4/4_2.js
@@ -1,18 +1,27 @@
 // С помощью прототипа реализовать структуру "Квадрат" которая будет наследоваться от структуры "Прямоугольник", 
 // должна быть возможность задавать размеры обеим фигурам и получать их площадь и периметр
 
-function Quadrilateral(side1, side2 = 0) {
+function Rectangle(side1, side2) {
   this.side1 = side1
-  side2 === 0 ? this.side2 = side1 : this.side2 = side2 
+  this.side2 = side2
 }
-Quadrilateral.prototype.perimetr = function() {return (this.side1 + this.side2) * 2} 
-Quadrilateral.prototype.area = function() {return (this.side1 * this.side2)} 
+function Square(side) {
+  this.side1 = this.side2 = side
+}
 
+function Calculator() {}
 
-rectangle = new Quadrilateral(5, 10)
+Calculator.prototype.perimetr = function() {return (this.side1 + this.side2)*2}
+Calculator.prototype.area = function() {return(this.side1 * this.side2) }
+
+Rectangle.prototype = new Calculator
+Square.prototype = new Calculator 
+
+rectangle = new Rectangle(5, 10)
+square = new Square(5) 
+
 console.log(rectangle.perimetr())
 console.log(rectangle.area())
 
-square = new Quadrilateral(15)
 console.log(square.perimetr())
 console.log(square.area())
--- lesson4/4_4.js
+++ lesson4/4_4.js
@@ -17,44 +17,70 @@
 
 // Верфи и корабли должны создаваться с помощью функций-конструкторов.
 
+function SailsShip (a,b) {
+  this.numberMasts = a 
+  this.areaSails = b
+}
 
-function ShipYard(type) {
-  this.type = type
+function MotorShip (a,b) {
+  this.powerEngine = a
+  this.hullMaterial = b
+}
 
+function MotorShipYard() {
+  this.build = function(a = 0, b = 0) {
+    return new MotorShip(a, b)
+  }
+
+  this.checkOfType = function(ship) { return ship instanceof MotorShip
+  } 
+}
+
+function YardMethods() {
   this.repair = function(ship) {
-    return ship.type === this.type? 'ship repaired': 'repair is not possible the type of shipyard does not match the type of ship' 
+    return this.checkOfType(ship)? 'ship repaired': 'repair is not possible the type of shipyard does not match the type of ship' 
   }  
   this.change = function(ship) {
-    return ship.type === this.type? new Ship(this.type, 0, 0): 'exchange is not possible the type of shipyard does not match the type of ship' 
+    return this.checkOfType(ship)? new ship.constructor(0, 0) : 'exchange is not possible the type of shipyard does not match the type of ship' 
   }
-
   this.paint = function(ship) {
-    return ship instanceof Ship? 'ship painted ': 'ship is not defined'  
-  }
-  this.build = function(a, b) {return new Ship(this.type, a, b)
+    return (ship instanceof SailsShip) || (ship instanceof MotorShip)? 'ship painted ': 'ship is not defined' 
   }  
 }
 
+MotorShipYard.prototype = new YardMethods
 
-Ship = function(type, a, b) {
-  this.type = type
-    if (this.type === 'sails') {this.numberMasts = a , this.areaSails = b 
-    }  
-    if (this.type === 'motor') {this.powerEngine = a, this.hullMaterial = b
-    }
+function SailsShipYard() {
+  this.build = function(a = 0, b = 0) {
+    return new MotorShip(a, b)
+  }
+
+  this.checkOfType = function(ship) { return ship instanceof SailsShip
+  } 
 }
 
-Ship.prototype = new ShipYard
+SailsShipYard.prototype = new YardMethods
 
-shipYardMotor = new ShipYard('motor')           // создание верфи для моторных судов
-shipYardSails = new ShipYard('sails')           // создание верфи для парусных судов
-console.log(shipYardSails)
-console.log(shipYardMotor)
-shipSails = shipYardSails.build(4, 1200)        // создание корабля на верфи с заданными параметрами
-shipMotor = shipYardMotor.build(1000,'stell')
-console.log(shipSails.type)
-console.log(shipMotor)
-console.log(shipYardMotor.paint(shipSails))     // покраска корабля любого типа любым типом верфи
-console.log(shipYardMotor.paint(shipSails))
-console.log(shipYardMotor.repair(shipSails))     // ремонт корабля только своим типом верфи
-console.log(shipYardMotor.change(shipSails))     // обмен корабля только своим типом верфи
+
+
+
+
+ship1 = new MotorShip(10, 'steel')
+ship2 = new SailsShip(10, 1000)
+console.log(ship2)
+console.log(ship1)
+motorShipYard = new MotorShipYard
+ship3 = motorShipYard.build(15, 'book')
+console.log(ship3)
+console.log(motorShipYard)
+console.log(motorShipYard.change(ship1))
+console.log(motorShipYard.repair(ship1))
+console.log(motorShipYard.paint(ship1))
+sailsShipYard = new SailsShipYard
+ship4 = motorShipYard.build(10, 1200)
+console.log(ship4)
+console.log(sailsShipYard)
+console.log(sailsShipYard.change(ship2))
+console.log(sailsShipYard.repair(ship2))
+console.log(sailsShipYard.paint(ship2))
+
