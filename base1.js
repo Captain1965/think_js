fbf8dd0a8e2ab14da8c127101ab8a4ddec623c94
Captain1965 <alex1965-03@mail.ru>
2022-08-28 12:34:06 +0300

    the task is implemented in files index1.html , base1.js

---
+++ base1.js
@@ -0,0 +1,28 @@
+var data = document.getElementById('item1')
+sample = {
+  title: 'Hello world',
+  description: 'The first program'
+}
+
+parseTemplate = function(data, sample) {
+  children = data.childNodes
+  for (let i = 0; i < children.length; i++) { 
+    try {
+      if (children[i].hasAttribute('data-field')) {
+        for (let key in sample) {
+          if (children[i].getAttribute('data-field') === key) {
+         children[i].textContent = sample[key]
+          } 
+        }
+      }
+    } 
+    catch(err){
+      console.log(err)
+    }  
+  }
+}
+
+parseTemplate(data,sample)
+
+
+ 
